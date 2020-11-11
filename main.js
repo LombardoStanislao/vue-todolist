989932f580f93b4571be48715293f8ff0ecdeb11
Stanislao <stanislaoweb@libero.it>
2020-11-11 11:39:25 +0100

    Inizio

---
+++ main.js
@@ -0,0 +1,24 @@
+// Vi consiglio di procedere per step:
+// definire una lista di todo e stamparli in pagina in un elenco
+// predisporre un input testuale per aggiungere un nuovo todo: al click di un pulsante, il testo inserito nell'input viene aggiunto alla lista dei todo
+// al click su un item della lista, il todo viene rimosso
+// BONUS: intercettare anche il tasto "ENTER" per aggiungere un nuovo todo
+
+var app = new Vue({
+
+  el: '#root',
+
+  data: {
+
+
+
+  },
+
+  methods: {
+
+
+
+  }, // Fine methods
+
+
+});
